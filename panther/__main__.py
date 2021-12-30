34c1325f23ac601a98f71f977bd79385af42948e
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-30 10:48:38 +0530

    Update __main__.py

--- panther/__main__.py
+++ panther/__main__.py
@@ -1,27 +1,26 @@
-# Copyright (c) 2021 ThePro-CoderZ
-# Part of: Nexa-Userbot
+
 import asyncio
 
 from pyrogram import idle
-from nexa_userbot import NEXAUB
-from nexa_userbot.modules import *
-from nexa_userbot.core.startup_checks import check_or_set_log_channel, check_arq_api
-from nexa_userbot.core.nexaub_database.nexaub_db_conf import get_log_channel
+from panther import panther
+from panther.plugins import *
+from panther.core.startup_checks import check_or_set_log_channel, check_arq_api
+from panther.core.panther_database.panther_conf import get_log_channel
 from config import Config
 
 
 async def main_startup():
     print("""
-|| Nexa Userbot ||
+|| Panther Userbot ||
 
-Copyright (c) 2021 ThePro-CoderZ
+Copyright (c) 2021 kaal0408
 """
     )
-    await NEXAUB.start()
+    await panther.start()
     log_channel_id = await check_or_set_log_channel()
     await check_arq_api()
     try:
-        await NEXAUB.send_message(chat_id=log_channel_id[1], text="`Nexa Userbot is started!`")
+        await panther.send_message(chat_id=log_channel_id[1], text="`panther Userbot is started!`")
     except:
         print("WARNING: There was an error while creating the LOG CHANNEL please add a one manually!")
     await idle()
