96b0d86e7dc6b50ebb3db22b55a12a2911c5c44d
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-31 10:32:57 +0530

    Add files via upload

---
+++ Panther/__init__.py
@@ -0,0 +1,27 @@
+
+import logging
+import sys
+import time
+from pyrogram import Client, errors
+from config import API_HASH, API_ID, SESSION
+import logging
+
+import logging
+
+logging.basicConfig(
+    filename="app.txt",
+    level=logging.ERROR,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+)
+LOGGER = logging.getLogger(__name__)
+
+HELP = {}
+CMD_HELP = {}
+
+StartTime = time.time()
+
+API_ID = API_ID
+API_HASH = API_HASH
+SESSION = SESSION
+
+app = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
---
+++ Panther/__main__.py
@@ -0,0 +1,11 @@
+
+from pyrogram import idle, Client, filters
+from config import PREFIX
+from Panther import app, LOGGER
+import logging
+from Zect.modules import *
+
+app.start()
+me = app.get_me()
+print(f"Panther UserBot started for user {me.id}. Type {PREFIX}help in any telegram chat.")
+idle()
