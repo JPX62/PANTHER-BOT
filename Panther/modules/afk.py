c0c8638f2b9bbb9cfcb49012c0d4a0b7e417bf43
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-01-10 10:39:58 +0530

    #define_time

--- Panther/modules/afk.py
+++ Panther/modules/afk.py
@@ -9,8 +9,46 @@ from config import PREFIX, LOG_CHAT
 from Panther.helpers.pyrohelper import get_arg
 import Panther.database.afkdb as Panther
 from Panther.helpers.pyrohelper import user_afk
-from Panther.modules.alive import get_readable_time
+
 from Panther.helpers.utils import get_message_type, Types
+
+def get_readable_time(seconds: int) -> str:
+
+    count = 0
+
+    ping_time = ""
+
+    time_list = []
+
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+
+        count += 1
+
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+
+        if seconds == 0 and remainder == 0:
+
+            break
+
+        time_list.append(int(result))
+
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+
+    if len(time_list) == 4:
+
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+
+    ping_time += ":".join(time_list)
+
+    return ping_time 
 
 CMD_HELP.update(
     {
